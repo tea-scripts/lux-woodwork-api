72e6012037a6c0cf476415d2186d8eac1e1ceaaf
teamie <timmieprince@gmail.com>
2022-10-14 18:04:16 +0800

    Order Controller Updated ✅

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -98,7 +98,7 @@ const getSingleOrder = async (req, res) => {
 const getAllOrders = async (req, res) => {
   const orders = await Order.find({}).populate(
     'user',
-    'username email first_name last_name'
+    'phone  email first_name last_name'
   );
   res.status(StatusCodes.OK).json({ orders });
 };
