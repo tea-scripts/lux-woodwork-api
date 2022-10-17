7556dfdd80237ae68c411b53afd6bd11257b8f74
teamie <timmieprince@gmail.com>
2022-10-18 03:27:19 +0800

    Order Pagination Limit Updated ✅

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -105,7 +105,7 @@ const getSingleOrder = async (req, res) => {
 
 const getAllOrders = async (req, res) => {
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 3;
+  const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
 
   const totalOrders = await Order.countDocuments();
