8bf02b2acc932c651d0fdf11cd58ca2765757207
rhaix23 <patrick.lloyd.santos@gmail.com>
2023-01-07 01:21:12 +0800

    Ticket Controlled Updated

--- controllers/supportTicketController.js
+++ controllers/supportTicketController.js
@@ -107,6 +107,21 @@ const cancelTicket = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "Ticket cancelled" });
 };
 
+const closeTicket = async (req, res) => {
+  const { id } = req.params;
+
+  const ticket = await SupportTicket.findById(id);
+
+  if (!ticket) {
+    throw new CustomError.NotFoundError(`Ticket not found`);
+  }
+
+  ticket.status = "closed";
+  await ticket.save();
+
+  res.status(StatusCodes.OK).json({ msg: "Ticket closed" });
+};
+
 const deleteTicket = async (req, res) => {
   const { id } = req.params;
 
@@ -127,4 +142,5 @@ module.exports = {
   deleteTicket,
   resolveTicket,
   cancelTicket,
+  closeTicket,
 };
